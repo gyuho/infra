61f94950883c15cc1087291287d35112227c5544
Gyuho Lee <gyuhox@gmail.com>
2023-11-19 00:01:37 +0800

    update
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- aws/go/s3/s3_test.go
+++ aws/go/s3/s3_test.go
@@ -3,9 +3,6 @@ package s3
 import (
 	"bytes"
 	"context"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -302,29 +299,4 @@ func TestS3Public(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-}
-
-func downloadFileToTmp(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("failed to download file: %s", resp.Status)
-	}
-
-	file := filepath.Join(os.TempDir(), randutil.AlphabetsLowerCase(10))
-	f, err := os.Create(file)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	_, err = io.Copy(f, resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return file, nil
 }
